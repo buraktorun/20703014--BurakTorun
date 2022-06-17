b8e686aa0a7ead693d19d506d43c1613ca12dd50
Burak Torun 20703014 <72433795+buraktorun@users.noreply.github.com>
2022-06-18 01:22:49 +0300

    wpf uygulaması

--- BTS206-20703014-BurakTorun/BTS206-20703014-BurakTorun/Properties/AssemblyInfo.cs
+++ BTS206-20703014-BurakTorun/BTS206-20703014-BurakTorun/Properties/AssemblyInfo.cs
@@ -1,6 +1,8 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Windows;
 
 // Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
 // denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
@@ -19,8 +21,25 @@ using System.Runtime.InteropServices;
 // erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
 [assembly: ComVisible(false)]
 
-// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
-[assembly: Guid("ed3c953a-4edc-4339-82db-5e9902142d45")]
+//Yerelleştirilebilir uygulamalar oluşturmaya başlamak için,
+//dosyanızdaki <UICulture>CultureYouAreCodingWith</UICulture> özelliğini
+//bir <PropertyGroup> grubuna ayarlayın.  Örneğin, kaynak dosyalarınızda ABD ingilizcesi
+//kullanıyorsanız, <UICulture> özelliğini en-US olarak ayarlayın. Sonra aşağıdaki
+//NeutralResourceLanguage özniteliğinin açıklamasını kaldırın.  Proje dosyasındaki
+//UICulture ayarıyla eşleşmesi için alt satırdaki "en-US"i güncelleyin.
+
+//[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //temaya özgü kaynak sözlüklerinin bulunduğu yer
+                                     //(bir kaynak sayfada
+                                     // uygulama kaynak sözlüklerinde bulunamazsa kullanılır)
+    ResourceDictionaryLocation.SourceAssembly //genel kaynak sözlüğünün bulunduğu yer
+                                              //(bir kaynak sayfada
+                                              // uygulama veya herhangi bir temaya özgü kaynak sözlüklerinde bulunamazsa kullanılır)
+)]
+
 
 // Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
 //
---
+++ BTS206-20703014-BurakTorun/BTS206-20703014-BurakTorun/product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTS206_20703014_BurakTorun
+{
+    public class product
+    {
+        public int Id { get; set; }
+        public string UrunAd { get; set; }
+        public decimal UrunFıyat { get; set; }
+        public int UrunMıktar { get; set; }
+        public string Umarka { get; set; }
+    }
+}
